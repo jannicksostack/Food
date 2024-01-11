57f0300fed5569a715fdd5c76f26c058183aa00a
mcdand <152167938+Naky762009@users.noreply.github.com>
2024-01-11 08:12:09 +0100

    Company repo

---
+++ madtilhjemlose/MVVM/DataAccess/CompanyRepository.cs
@@ -0,0 +1,30 @@
+using madtilhjemlose.MVVM.Model;
+
+namespace madtilhjemlose.MVVM.DataAccess;
+
+public class CompanyRepository : BaseRepository
+{
+
+    //public class CompanyRepository()
+    //{
+
+    //}
+
+    //public Company Create(int id, string name, string address)
+    ///throw new NotImplementedException();
+    //}
+    //public List<Company> GetCompanies()
+    //{
+    //throw new NotImplementedException();
+    //}
+    //public void UpdateCompany(Company company)
+    //{
+    //throw new NotImplementedException();
+    //}
+    //}
+}
+
+
+
+
+
