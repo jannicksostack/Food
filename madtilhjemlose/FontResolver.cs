0297b2fadd8352aa6cb06759823b89445147c84a
jannicksostack <142694256+jannicksostack@users.noreply.github.com>
2024-01-11 11:27:04 +0100

    Pdf font virker. Har lavet FontResolver

---
+++ madtilhjemlose/FontResolver.cs
@@ -0,0 +1,28 @@
+﻿using PdfSharp.Fonts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace madtilhjemlose
+{
+    public class FontResolver : IFontResolver
+    {
+        public byte[]? GetFont(string faceName)
+        {
+            Assembly assembly = Assembly.GetAssembly(typeof(FontResolver));
+            string name = assembly.GetManifestResourceNames().First(x => x.Contains("Regular.ttf"));
+            using Stream s = assembly.GetManifestResourceStream(name);
+            using MemoryStream mem = new();
+            s.CopyTo(mem);
+            return mem.ToArray();
+        }
+
+        public FontResolverInfo? ResolveTypeface(string familyName, bool isBold, bool isItalic)
+        {
+            return new FontResolverInfo("test");
+        }
+    }
+}
